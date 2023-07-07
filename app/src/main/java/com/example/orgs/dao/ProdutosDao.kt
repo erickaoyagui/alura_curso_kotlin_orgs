a90b737a23887f23e53a2f04f5403573501be05e
Erick Aoyagui <erickaoyagui@hotmail.com>
2023-07-07 13:08:38 -0300

    Curso de Kotlin Encerrado

---
+++ app/src/main/java/com/example/orgs/dao/ProdutosDao.kt
@@ -0,0 +1,18 @@
+package com.example.orgs.dao
+
+import com.example.orgs.model.Produto
+
+class ProdutosDao {
+
+    fun adiciona(produto: Produto){
+        Companion.produtos.add(produto)
+    }
+
+    fun buscaTodos(): List<Produto>{
+        return Companion.produtos.toList()
+    }
+
+    companion object {
+        private val produtos = mutableListOf<Produto>()
+    }
+}
